16f07bfd760587638f8415b8da83ca9b4d5a166d
Ngoc Giang <giang.ngocdao@gmail.com>
2020-07-29 10:03:14 +0700

    add FacebookCounter style arguments

--- src/components/facebook/FacebookCounter.js
+++ src/components/facebook/FacebookCounter.js
@@ -1,69 +1,70 @@
-import React from 'react'
-import reactCSS from 'reactcss'
-import _ from 'lodash'
-import { listOfNames } from '../../helpers/strings'
+import React from "react";
+import reactCSS from "reactcss";
+import _ from "lodash";
+import { listOfNames } from "../../helpers/strings";
 
-import FacebookCounterReaction from './FacebookCounterReaction'
+import FacebookCounterReaction from "./FacebookCounterReaction";
 
-export const FacebookCounter = ({ counters, user, important, onClick, bg }) => {
+export const FacebookCounter = ({
+  counters,
+  user,
+  important,
+  onClick,
+  bg,
+  styleCounter,
+  styleName
+}) => {
   const styles = reactCSS({
-    'default': {
+    default: {
       counter: {
-        display: 'flex',
-        cursor: 'pointer',
-        color: '#365899',
-        fontFamily: `"San Francisco", -apple-system, BlinkMacSystemFont,
-          ".SFNSText-Regular", sans-serif`,
-        fontSize: '12px',
-        fontWeight: '500',
+        display: "flex",
+        cursor: "pointer"
       },
       name: {
-        paddingLeft: '4px',
-        marginTop: '2px',
-      },
-    },
-  })
+        paddingLeft: "4px"
+      }
+    }
+  });
 
-  const groups = _.groupBy(counters, 'emoji')
-  const names = _.map(counters, 'by')
+  const groups = _.groupBy(counters, "emoji");
+  const names = _.map(counters, "by");
 
-  const nameString = []
+  const nameString = [];
   if (_.includes(names, user)) {
-    nameString.push('You')
+    nameString.push("You");
   }
   if (important.length) {
     if (_.includes(names, important[0])) {
-      nameString.push(important[0])
+      nameString.push(important[0]);
     }
     if (_.includes(names, important[1])) {
-      nameString.push(important[1])
+      nameString.push(important[1]);
     }
   }
-  nameString.push(`${ names.length - nameString.length } others`)
-
+  nameString.push(`${names.length - nameString.length} others`);
 
   return (
-    <div style={ styles.counter } onClick={ onClick }>
-      { _.map(_.keys(groups), (reaction, i, reactions) => {
+    <div style={[styles.counter, styleCounter]} onClick={onClick}>
+      {_.map(_.keys(groups), (reaction, i, reactions) => {
         return (
           <FacebookCounterReaction
-            key={ i }
-            reaction={ reaction }
-            index={ reactions.length - i }
-            bg={ bg }
+            key={i}
+            reaction={reaction}
+            index={reactions.length - i}
+            bg={bg}
           />
-        )
-      }) }
-      <div style={ styles.name }>
-        { listOfNames(nameString) }
-      </div>
+        );
+      })}
+      <div style={[styles.name, styleName]}>{listOfNames(nameString)}</div>
     </div>
-  )
-}
+  );
+};
 
 FacebookCounter.defaultProps = {
   important: [],
-  bg: '#fff',
-}
+  bg: "#fff",
+  styleCounter: [],
+  styleName: []
+};
 
-export default FacebookCounter
+export default FacebookCounter;
