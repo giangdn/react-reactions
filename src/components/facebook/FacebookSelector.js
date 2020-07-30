8595c2ff88256dd65e10ea638f85749278c7cec9
Ngoc Giang <giang.ngocdao@gmail.com>
2020-07-30 18:07:33 +0700

    update FacebookSelector for custom icon and style

--- src/components/facebook/FacebookSelector.js
+++ src/components/facebook/FacebookSelector.js
@@ -1,47 +1,62 @@
-import React from 'react'
-import reactCSS from 'reactcss'
-import _ from 'lodash'
-import icons from '../../helpers/icons'
+import React from "react";
+import reactCSS from "reactcss";
+import _ from "lodash";
+import icons from "../../helpers/icons";
 
-import FacebookSelectorEmoji from './FacebookSelectorEmoji'
+import FacebookSelectorEmoji from "./FacebookSelectorEmoji";
 
-export const FacebookSelector = ({ iconSize, reactions, variant, onSelect }) => {
+export const FacebookSelector = ({
+  iconSize,
+  reactions,
+  variant,
+  onSelect,
+  customIcons,
+  styleSelector
+}) => {
   const styles = reactCSS({
-    'default': {
+    default: {
       selector: {
-        backgroundColor: '#fff',
-        borderRadius: '50px',
-        padding: '2px',
-        boxShadow: '0 0 0 1px rgba(0, 0, 0, .05), 0 1px 2px rgba(0, 0, 0, .15)',
-        display: 'flex',
+        backgroundColor: "#fff",
+        borderRadius: "50px",
+        padding: "2px",
+        boxShadow: "0 0 0 1px rgba(0, 0, 0, .05), 0 1px 2px rgba(0, 0, 0, .15)",
+        display: "flex"
       },
       icon: {
-        width: `${ iconSize + 10 }px`,
-      },
-    },
-  })
+        width: `${iconSize + 10}px`
+      }
+    }
+  });
+
+  if (typeof styleSelector !== "undefined") {
+    styles.selector = Object.assign(styles.selector, styleSelector);
+  }
 
   return (
-    <div style={ styles.selector }>
-      { _.map(reactions, (reaction) => {
+    <div style={styles.selector}>
+      {_.map(reactions, reaction => {
         return (
-          <div style={ styles.icon } key={ reaction }>
+          <div style={styles.icon} key={reaction}>
             <FacebookSelectorEmoji
-              icon={ icons.find(variant, reaction) }
-              label={ reaction }
-              onSelect={ onSelect }
+              icon={
+                typeof customIcons !== "undefined"
+                  ? customIcons.find(variant, reaction)
+                  : icons.find(variant, reaction)
+              }
+              label={reaction}
+              onSelect={onSelect}
             />
           </div>
-        )
-      }) }
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 FacebookSelector.defaultProps = {
-  reactions: ['like', 'love', 'haha', 'wow', 'sad', 'angry'],
+  reactions: ["like", "love", "haha", "wow", "sad", "angry"],
   iconSize: 38,
-  variant: 'facebook',
-}
+  variant: "facebook"
+};
 
-export default FacebookSelector
+export default FacebookSelector;
